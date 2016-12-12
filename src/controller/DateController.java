54b72c7395505f95a98e62fdd57386231fb38057
Mikhail Sobol <mikhailsobolemail@gmail.com>
2016-12-12 22:53:15 +0200

    Date pattern changed.

--- src/controller/DateController.java
+++ src/controller/DateController.java
@@ -8,7 +8,7 @@ import java.util.Date;
 public class DateController {
 
     public static String getCurrentDate() {
-        final DateFormat dateFormat = new SimpleDateFormat("yy_MM_dd");
+        final DateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd");
         final Date date = new Date();
         return dateFormat.format(date);
     }
